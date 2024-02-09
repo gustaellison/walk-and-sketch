a8151169693a19d719980ae58d93855512ff89ba
gustaellison <gustaellison@gmail.com>
2024-02-09 11:03:04 -0500

    updating tour model

--- models/Ticket.js
+++ models/Ticket.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const ticketSchema = new Schema({
     date: { type: String, required: true },
     _tour: { type: Schema.Types.ObjectId, ref: 'Tour', required: true },
-    _users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }]
+    _users: { type: Schema.Types.ObjectId, ref: "User", required: true }
     }, { timestamps: true })
 
     ticketSchema.index({ date: 1, _tour: 1, _users: 1 });
